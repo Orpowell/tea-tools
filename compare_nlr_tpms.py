e3292ed07cd5b2f82c9514e18c3584073cf6595e
powellor <oliver.powell@kaust.edu.sa>
2025-03-06 18:05:59 +0300

    Updated visualisation fucntion

--- compare_nlr_tpms.py
+++ compare_nlr_tpms.py
@@ -3,22 +3,35 @@ from Bio import SeqIO
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
+import csv
 
 def load_nlr_transcripts(transcripts: str) -> list[str]:
     with open(transcripts) as file:
         return [line.strip() for line in file]
 
-def visualise_nlr_transcripts(transcripts: str, quantification: str, gene_name: str, graph_out:str, gene_quant: int):
+def load_transcript_quantification(quants: str) -> dict[str, float]:
+
+    with open(quants) as file:
+
+        reader = csv.reader(file, delimiter="\t")
+
+        next(reader)
+
+        return {row[0]:float(row[3]) for row in reader }
+
+
+def visualise_nlr_transcripts(transcripts: str, quantification: str, graph_out:str, genes_of_interest=[str], aliases=[str]):
     nlrs = load_nlr_transcripts(transcripts=transcripts)
 
-    df = pd.read_csv(quantification, sep="\t")
-    nlrs = df[(df.Name.isin(nlrs))]
+    transcript_quants = load_transcript_quantification(quants=quantification)
+    
+    nlrs = {k:v for k,v in transcript_quants.items() if k in nlrs}
 
+    nlr_tpm = list(nlrs.values())
 
-    q75 = np.quantile(nlrs.TPM, 0.75)
+    q75 = np.quantile(nlr_tpm, 0.75)
 
-    counts, bins = np.histogram(nlrs.TPM, bins=150, )
+    counts, bins = np.histogram(nlr_tpm, bins=150, )
 
     colours = []
 
@@ -43,7 +56,7 @@ def visualise_nlr_transcripts(transcripts: str, quantification: str, gene_name: 
     ax.set_xlabel('Transcripts Per Million TPM')  # Set the label for the x-axis
     ax.set_ylabel('Frequency')  # Set the label for the y-axis
     ax.spines['bottom'].set_position(('data', -5))
-    ax.set_ylim(-5, max(counts))
+    #ax.set_ylim(-5, max(counts))
     ax.set_xlim(-2,)
     ax.spines[['right', 'top']].set_visible(False)
 
@@ -51,9 +64,21 @@ def visualise_nlr_transcripts(transcripts: str, quantification: str, gene_name: 
     b75 = mpatches.Patch(color='grey', label='Bottom 75%')
     t25 = mpatches.Patch(color='orange', label='Top 25%')
 
-    ax.annotate(fr'{gene_name}', xy=(gene_quant, 1), xytext=(10, 50), textcoords='offset points',
-                    arrowprops=dict(arrowstyle='-|>', color='black'), style='italic')
+    gene_names = dict(zip(genes_of_interest, genes_of_interest))
 
+    if len(genes_of_interest) == len(aliases):
+        gene_names = dict(zip(genes_of_interest, aliases))
+    
+
+    i = 50
+    for gene in genes_of_interest:
+
+
+        
+        
+        ax.annotate(fr'{gene_names[gene]}', xy=(transcript_quants[gene], 1), xytext=(i, i), textcoords='offset points',
+                        arrowprops=dict(arrowstyle='-|>', color='black'), style='italic')
+        i+=50
 
     plt.legend(handles=[t25, b75], title="NLR Expression")
     plt.tight_layout()
@@ -72,8 +97,6 @@ def get_top25(transcripts: str, transcriptome: str, quantification: str, basenam
         for name, seq in nlr_transcripts.items():
             out.write(f">{name}\n")
             out.write(f"{seq}\n")
-
-    
 
     df = pd.read_csv(quantification, sep="\t")
     nlrs_df = df[(df.Name.isin(nlrs))]
@@ -102,36 +125,53 @@ def get_top25(transcripts: str, transcriptome: str, quantification: str, basenam
 
 if __name__ == "__main__":
     
+    
     """
-    nlr_transcripts = "/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA1675_NLR_trancripts.txt"
-    bf_transcripts = "/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA1675_beta_finger_trancripts.txt"
-    quantification = "/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA1675/TA1675_quantification/quant.sf"
-    visualise_nlr_transcripts(transcripts=nlr_transcripts, quantification=quantification, gene_name="Yr28", graph_out="Yr28_expression.png", gene_quant=10.195096)
-    visualise_nlr_transcripts(transcripts=nlr_transcripts, quantification=quantification, gene_name="Sr46", graph_out="Sr46_TRINITY_DN1954_c0_g1_i12_expression.png", gene_quant=15.449350)
-    visualise_nlr_transcripts(transcripts=bf_transcripts, quantification=quantification, gene_name="Lr39", graph_out="Lr39_TRINITY_DN857_c1_g1_i2_expression.png", gene_quant=26.351422)
-    visualise_nlr_transcripts(transcripts=bf_transcripts, quantification=quantification, gene_name="WTK4", graph_out="WTK4_TRINITY_DN2349_c0_g2_i1_expression.png", gene_quant=27.023780)
-    visualise_nlr_transcripts(transcripts=bf_transcripts, quantification=quantification, gene_name="WTK4", graph_out="WTK4_TRINITY_TRINITY_DN305_c0_g1_i41_expression.png", gene_quant=6.643938)
-    """
+    #visualise_nlr_transcripts(transcripts=nlr_transcripts, quantification=quantification, gene_name="Yr28", graph_out="Yr28_expression.png", gene_quant=10.195096)
+    #visualise_nlr_transcripts(transcripts=nlr_transcripts, quantification=quantification, gene_name="Sr46", graph_out="Sr46_TRINITY_DN1954_c0_g1_i12_expression.png", gene_quant=15.449350)
 
-    """
+    visualise_nlr_transcripts(transcripts="/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA1675_beta_finger_trancripts.txt", 
+                              quantification="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA1675/TA1675_quantification/quant.sf", 
+                              graph_out="TA1675_cloned_r_genes.png", 
+                              genes_of_interest=["TRINITY_DN857_c1_g1_i2", "TRINITY_DN2349_c0_g2_i1", "TRINITY_DN305_c0_g1_i41"],
+                              aliases=["Lr39", "WTK4_1", "WTK4_2"]
+                              )     
+
+
+
+    # Cloned KFPs in TA1675 
+    visualise_nlr_transcripts(transcripts="/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA1675_beta_finger_trancripts.txt", 
+                              quantification="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA1675/TA1675_quantification/quant.sf", 
+                              graph_out="TA1675_cloned_r_genes.png", 
+                              genes_of_interest=["TRINITY_DN857_c1_g1_i2", "TRINITY_DN2349_c0_g2_i1", "TRINITY_DN305_c0_g1_i41"],
+                              aliases=["Lr39", "WTK4_1", "WTK4_2"]
+                              )    
+
+
     TOWWC054
     transcripts = "/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TOWWC054_NLR_trancripts.txt"
     quantification = "/home/powellor/Documents/projects/r_gene_expression_david/analysis/TOWWC054_quant/TOWWC054_salmon_out/quant.sf"
-    visualise_nlr_transcripts(transcripts=transcripts, quantification=quantification, gene_name="SrTA10187", graph_out="SrTA10187_expression.png", gene_quant=13.496783)
-    """
+    visualise_nlr_transcripts(transcripts=transcripts, 
+                              quantification=quantification, 
+                              gene_name="SrTA10187", 
+                              graph_out="SrTA10187_expression.png",
+                              gene_quant=13.496783)
 
-    """
+
+    
     #TA10171
-    transcriptome = "/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA10171/trinity_TA10171.Trinity.fasta"
-    transcripts = "/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA10171_NLR_trancripts.txt"
-    quantification = "/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA10171/TA10171_quantification/quant.sf"
-    visualise_nlr_transcripts(transcripts=transcripts, quantification=quantification, gene_name="DN2103_c0_g2_i7", graph_out="TA10171_DN2103_c0_g2_i7_expression.png", gene_quant=6.709618)
-    visualise_nlr_transcripts(transcripts=transcripts, quantification=quantification, gene_name="Dn2103_c0_g3_i2", graph_out="TA10171_DN2103_c0_g2_i7_expression.png", gene_quant=4.758894)
-    """
+    visualise_nlr_transcripts(transcripts = "/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TA10171_NLR_trancripts.txt", 
+                              quantification="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TA10171/TA10171_quantification/quant.sf", 
+                              graph_out="TA10171_expression.test.png",
+                              genes_of_interest=["TRINITY_DN2103_c0_g2_i7","TRINITY_DN2103_c0_g3_i2"],
+                              )
+    
     
     
     #TOWWC0107
-    get_top25(transcriptome="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TOWWC0107/trinity_TOWWC0107.Trinity.fasta", transcripts="/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TOWWC017_NLR_trancripts.txt", quantification="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TOWWC0107/TOWWC0107_quantification/quant.sf", basename="TOWWC0107_nlrs")
-    
-    
-
+    visualise_nlr_transcripts(transcripts="/home/powellor/Documents/projects/r_gene_expression_david/scripts/nlr_quant_scripts/TOWWC0107_NLR_trancripts.txt", 
+                            quantification="/home/powellor/Documents/projects/r_gene_expression_david/analysis/TOWWC0107/TOWWC0107_quantification/quant.sf", 
+                            graph_out="TOWWC0107_candidates.png", 
+                            genes_of_interest=["TRINITY_DN1105_c0_g1_i7", "TRINITY_DN1105_c0_g1_i6"],
+                            )
+    """
